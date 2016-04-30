c0c907e3e7609a0155139cbbf3b59f84d4304c5b
Philippa Karoly <philippa.karoly@gmail.com>
2016-04-30 10:21:24 +1000

    update challenges

--- challenges/loops_challenges.m
+++ challenges/loops_challenges.m
@@ -11,7 +11,7 @@
 % Change your loop so that it only displays the sums on days 4,7,13,16,28
 % and 33.
 
-%% Extension
+%% Challenge 1 - Extension
 % Modify your loop to instead PLOT the inflammation data of ALL patients on days 4,7,13,16,28 and 33 
 % Use subplots with two rows and three columns
 % Make sure each of your subplot has a title that indicates the day.
@@ -24,23 +24,9 @@
 % and saves out a heatmap for each data file (using imagesc).
 % HINT: loop using the variable idx.
 
-%% Extension
-
 %% Challenge 3
 
-% Copy and paste the code in the section below to the end of your earlier
-% script - pie_script.m and uncomment it
-
-%%
-% for bites = 1:HowHungryIAm 
-%   PieLeftover = PieLeftover - SizeOfBite;
-%   PercentPieLeftover = PieLeftover / Pie * 100
-%   disp('there is ' num2str(PieLeftover) ' pie leftover')) 
-%   disp(['there is ' numtostr(bites*SizeofBite) ' pie in my stomach'])
-%   disp(['You ate ' 100-PercentPieLeftover '% of the pie']);
-
-% % no time to debug code, must find pie...mmmmmmmm pie.... 
-% % this is the end of the code
+% Open pie_script2.m and debug it
 
 %% Challenge 3 - Extension
 
