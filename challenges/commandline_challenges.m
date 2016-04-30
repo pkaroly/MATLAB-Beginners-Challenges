3584eb19b28736ad2a5566b53c83c158e5bb62ba
Philippa Karoly <philippa.karoly@gmail.com>
2016-04-30 10:17:43 +1000

    update challenges

--- challenges/commandline_challenges.m
+++ challenges/commandline_challenges.m
@@ -6,44 +6,8 @@
 
 
 %% Challenge 2
-% Create a 8 X 8 array R by copying by running the following line
+% CHALLENGE TWO
 
-R = rand(8, 8); 
-
-% Challenge Question: What does the command rand do? What about randi?
-
-
-%% Challenge 2 - Part 1
-% Write code to:
-
-% Display the entire array.
-
-% Assign the third row to a variable of your choice (eg: 'T').
-
-% Assign the sixth column to a variable of your choice (eg: 'S').
-
-% Challenge Question: What does it look like if you run
-R(:);
-
-
-%% Challenge 2 - Part 2
-% Display the following sentence:
-% 'The 5th element of the 5th column is 0.8605'
-% (The actual number will vary because the rand function generates a different
-% array everytime. In this case, your number should reflect the number in
-% the 5th row of the 5th column.) Use indexing.
-
-%% Challenge 2 - Part 3
-% Transpose the vectors T and S and assign them to the variable T_transpose 
-% and  S_transpose. 
-% Hint: search the help documentation for transpose.
-
-%% Challenge 2 - Part 4
-
-% Horizontally Concatenate the two row vectors T and S_transpose.
-% Vertically Concatenate the two column vectors S and T_transpose.
-% Horizontally Concatenate the two column vectors S and T_transpose.
-% Hint: search the help documentation for 'Concatenate'
 
 %% Challenge 2 - Extension
 % MATLAB is most useful for dealing with numbers, so we haven't 
@@ -52,8 +16,6 @@ R(:);
 % Create a string with your celebrity name in it and call it my_name
 % (HINT your celebrity name is your middle name followed by the name of the first
 % street you lived on)
-
-
 
 % Create a new variable called my_code_name, which contains only
 % every second letter of my_name
@@ -65,35 +27,17 @@ R(:);
 
 example_code_name = fliplr(my_name);
 
-%% Challenge 3 - Part 3
-
+%% Challenge 3 
 % You saw how to calculate the mean inflammation for different
 % dimensions
 % Now create a variable that contains the maximum inflammation on each day
 % and another with the maximum for each patient
-
+ 
 % HINT: Using max on different dimensions is not exactly the same as mean -
 % check the documentation!!
 
 
-%% Challenge 3 - Extension
-%  Repeat Part 4 for min and std. 
-
-
-% Think of another statistic you might like to know and search
-% for it in the documentation
-
-
-%% Challenge 4
-% Create a plot showing the standard deviation of the inflammation data
-% for each day
-
-
-%% Challenge 4 - Extension
-% Plotting using subplots
-
-% Type help subplot in the command window or search the documentation
-% Can you work out how to create two plots in the same figure?
-
-% Plot the inflammation values of two different patients side by side
-% Don't forget to add axis labels and a title to each plot
+%% Challenge 3 - EXTENSION 
+% Plot your result
+ 
+%  Repeat for min and std. 
--- challenges/writingscripts_challenge.m
+++ challenges/writingscripts_challenge.m
@@ -1,27 +1,37 @@
 %% Writing Scripts
 
 %% Challenge #1
-% Plot mean, min, max and std calculated over all patients for each day in 
-% a subplot. Arrange your subplot so that you have two plots in the first 
-% row and two in the second row.
+% Using analyse_data as as model, create your own script called
+% plot_data that plots mean, min, max and std calculated over all patients for each day.
+
+%% Challenge 1 - Extension
+% Plotting using subplots
+ 
+% Type help subplot in the command window or search the documentation
+% Can you work out how to create two plots in the same figure?
+ 
+% Plot the four plots you have generated with your analyse.data script, but now arrange them in a subplot so that you have two plots in the first 
+% row and two in the second row
 % Give all subplots x and y labels and a title.
+
 
 %% Challenge #2
 % Add a line to your script that will save the plot you generated 
 % as a png file in the current working directory.
 
-%% Extension
+%% Challenge 2 - Extension
 % Using the 'hold' function plot the mean, minimum and maximum inflammation
 % in the same figure. See if you can use different colours to differentiate
 % between the different parameters. Add a legend to your figure. Save your
 % figure as a png.
-
+ 
 % Look at the documentation for print and see if you can work out how to
 % save your plot in a different format
-
+ 
 % Can you also work out how to save it at a higher resolution?
+
 
 %% Challenge #3
 % Debugging scripts
 
-% Debug pie_script
+% Open pie_script1.m and debug it
