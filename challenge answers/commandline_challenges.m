56d3dd51d4b95a927beda4c05ee62089cec61cb2
Philippa Karoly <pkaroly@student.unimelb.edu.au>
2016-05-02 11:36:32 +1000

    answers part1

--- challenge answers/commandline_challenges.m
+++ challenge answers/commandline_challenges.m
@@ -4,9 +4,9 @@
 % plus 10.
 % Make another variable (Choose a name) and set it equal to half your age.
 
-x = 3;
-y = 4;
-z = x + y;
+age_2015 = 26;
+age_2025 = age_2015 + 10;
+age_half = age_2015 / 2;
 
 %% Challenge 2
 % create a square matrix in your workspace that is filled with random
@@ -17,7 +17,7 @@ z = x + y;
 % randi([-10 10],2,3) is a matrix with 2 rows, and 3 columns,
 % containing numbers between -10 and 10
 
-% try it out! (highlight the following line and press F9)
+% try it out! (highlight the following line and press F9 - or right click & evaluate selection on a mac)
 random_matrix = randi([-10 10],2,3)
 
 % __________ PART ONE ___________
@@ -62,15 +62,15 @@ big_matrix = [random_matrix, random_matrix2]
 % Create a string with your celebrity name in it and call it my_name
 % (HINT your celebrity name is your middle name followed by the name of the first
 % street you lived on)
+my_name = 'jane waterloo';
 
 % Create a new variable called my_code_name, which contains only
 % every second letter of my_name
 % (HINT: remember how to index in steps?)
-
+my_code_name = my_name(1:2:end);
 
 % Create your own code (for an example
 % see what fliplr does to my_name)
-
 example_code_name = fliplr(my_name);
 
 %% Challenge 3 
@@ -83,9 +83,35 @@ example_code_name = fliplr(my_name);
 % HINT: Using max on different dimensions is 
 % not exactly the same as mean -
 % check the documentation!!
+PatientData = csvread('inflammation-01.csv');
+
+% the maximum for each day(ie dimension 1)
+DailyMax  = max(PatientData);
+DailyMax = max(PatientData,[],1);
+
+% the maximum for each patient (ie taking the maximum along the days - or
+% dimension 2)
+PatientMax = max(PatientData,[],2);
 
 
 %% Challenge 3 - EXTENSION 
 % Plot your result
+
+% this is a heatmap
+figure; % open a figure window
+imagesc(PatientData);
+
+% this is the daily max as a line plot
+figure;  % open a new figure window
+plot(DailyMax);
+
+% this is the patient max as a line plot
+figure;
+plot(PatientMax);
  
 %  Repeat for min and std. 
+figure; 
+plot(min(PatientData,[],1));
+
+figure;
+plot(std(PatientData,0,1));
